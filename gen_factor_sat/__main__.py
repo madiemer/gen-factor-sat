911630eaffa16bc72d3f1a3082139a1705e42979
Maximilian Diemer <maximilian.diemer@uni-ulm.de>
2020-11-29 11:37:59 +0100

    Add default filename

--- gen_factor_sat/__main__.py
+++ gen_factor_sat/__main__.py
@@ -3,28 +3,43 @@ import sys
 
 from gen_factor_sat import factoring_sat
 
-commands = ['number', 'random']
-
 parser = argparse.ArgumentParser()
-parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
 
-# Commands
+commands = ['number', 'random']
 subparsers = parser.add_subparsers(dest='command', required=True)
 
 parser_number = subparsers.add_parser(commands[0])
 parser_number.add_argument('value', type=int)
+parser_number.add_argument('-o', '--outfile', nargs='?', type=str, const='', default='-')
 
 parser_random = subparsers.add_parser(commands[1])
 parser_random.add_argument('-s', '--seed', nargs='?', type=int)
 parser_random.add_argument('max_value', metavar='max-value', type=int)
+parser_random.add_argument('-o', '--outfile', nargs='?', type=str, const='', default='-')
 
 args = parser.parse_args()
 
+
+def write_cnf(cnf, filename, default_file):
+    if filename == '-':
+        sys.stdout.write(cnf.to_dimacs())
+    else:
+        if not filename:
+            filename = default_file
+
+        with open(filename, 'w') as file:
+            file.write(cnf.to_dimacs())
+
+
 if args.command == commands[0]:
     result = factoring_sat.factorize_number(args.value)
-    args.outfile.write(result.to_dimacs())
+    default = 'factor_number{0}.cnf'.format(result.number)
+    write_cnf(result, args.outfile, default)
+
 elif args.command == commands[1]:
     result = factoring_sat.factorize_random_number(args.max_value, args.seed)
-    args.outfile.write(result.to_dimacs())
+    default = 'factor_seed{0}_maxn{1}.cnf'.format(result.seed, result.max_value)
+    write_cnf(result, args.outfile, default)
+
 else:
     parser.error('Invalid command: ' + str(args.command))
